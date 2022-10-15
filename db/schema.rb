17ce739a002150343cfd89772b7b70f72c7a1039
Matheus Zambone <matheus@zambonecomercial.com>
2022-10-15 11:58:39 -0300

    model launch criado

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_15_120301) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_15_145724) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -22,6 +22,12 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_15_120301) do
     t.string "newsSite"
     t.string "summary"
     t.string "publishedAt"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "launches", force: :cascade do |t|
+    t.string "provider"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
