2d67bc3138a683d8e31f87c4ed8613434833d0d7
Matheus Zambone <matheus@zambonecomercial.com>
2022-10-15 11:03:52 -0300

    Article controller e métodos funcionando perfeitamente

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,4 +4,43 @@ class ArticlesController < ApplicationController
 
     render json: @articles
   end
+
+  def show
+    @article = Article.find params[:id]
+
+    render json: @article
+  end
+
+  def create
+    @article = Article.new article_params
+
+    if @article.save
+      render json: @article, status: :created
+    else
+      render json: @article.errors, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    @article = Article.find params[:id]
+
+    if @article.update article_params
+      render json: @article
+    else
+      render json: @article.errors, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @article = Article.find params[:id]
+    @article.destroy
+
+    head :ok
+  end
+
+  private
+
+  def article_params
+    params.permit(:featured, :title, :url, :imageUrl, :newsSite, :summary, :publishedAt)
+  end
 end
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,8 @@
 class Article < ApplicationRecord
+  validates :title, presence: true
+  # validates :url, presence: true
+  # validates :imageUrl, presence: true
+  # validates :newsSite, presence: true
+  # validates :summary, presence: true
+  # validates :publishedAt, presence: true
 end
